fafed351cd670938f2b2bca892d975a751b0844f
Altan Emre <altan_emre_1989@hotmail.com>
2022-10-08 21:45:16 +0300

    CategoryManager Continue..

---
+++ ERP.BusinessLogicLayer/CategoryManager.cs
@@ -0,0 +1,45 @@
+﻿using ERP.DataAccessLayer;
+using ERP.DataAccessLayer.EF;
+using ERP.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.BusinessLogicLayer
+{
+    public class CategoryManager
+    {
+        private Repository<Category> repo_category = new Repository<Category>();
+
+        public List<Category> GetAll()
+        {
+            return repo_category.List();
+        }
+        public int Insert(Category entity)
+        {
+            return repo_category.Insert(entity);
+        }
+
+        public Category Find(Expression<Func<Category, bool>> where)
+        {
+            using(var context = new DatabaseContext())
+            {
+                var category = context.Categories.Include("Products").Where(where).FirstOrDefault();
+
+                return category;
+            }           
+        }
+
+        public int Update(Category model)
+        {
+            return repo_category.Update(model);
+        }
+        public int Delete(Category cat)
+        {            
+            return repo_category.Delete(cat);
+        }
+    }
+}
---
+++ ERP.WEBUI/Controllers/CategoryController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ERP.BusinessLogicLayer;
+using ERP.Entity;
+
+namespace ERP.WEBUI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private CategoryManager categoryManager = new CategoryManager();
+
+        // GET: Category
+        public ActionResult Index()
+        {
+            return View(categoryManager.GetAll());
+        }
+
+        // GET: Category/Details/5
+        //public ActionResult Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Category category = db.Categories.Find(id);
+        //    if (category == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(category);
+        //}
+
+   
+        public ActionResult Create()
+        {
+            return View(new Category());
+        }
+
+  
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Category category)
+        {
+            ModelState.Remove("ModifiedUsername");
+            if (ModelState.IsValid)
+            {
+                categoryManager.Insert(category);
+                return RedirectToAction("Index");
+            }
+
+            return View(category);
+        }
+
+        // GET: Category/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = categoryManager.Find(cat => cat.Id==id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+               
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Category cat)
+        {
+            ModelState.Remove("ModifiedUsername");
+            if (ModelState.IsValid)
+            {
+                Category category = categoryManager.Find(c => c.Id == cat.Id);
+
+                if (category == null)
+                {
+                    return HttpNotFound();
+                }
+
+                category.Name = cat.Name;
+                category.Description = cat.Description;                
+
+                int sonuc = categoryManager.Update(category);
+
+                return RedirectToAction("Index");
+            }
+            return View(cat);
+        }
+
+        // GET: Category/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = categoryManager.Find(i=> i.Id==id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Category/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Category category = categoryManager.Find(i => i.Id == id);
+            categoryManager.Delete(category);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
